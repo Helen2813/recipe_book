7ebd34e6ae12836d8448a13f24d27f6dbb4ba8a3
Elena Krikun <elena.krikun@quantori.com>
2022-11-17 14:49:30 +0400

    implemented routes

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Ingredient } from '../shared/ingredient.model';
-import {ShoppingListService} from "./shopping-list.service";
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
@@ -9,16 +10,16 @@ import {ShoppingListService} from "./shopping-list.service";
 })
 export class ShoppingListComponent implements OnInit {
   ingredients: Ingredient[];
-
+  
   constructor(private slService: ShoppingListService) { }
-
-  ngOnInit(): void {
+  
+  ngOnInit() {
     this.ingredients = this.slService.getIngredients();
     this.slService.ingredientChanged
       .subscribe(
         (ingredients: Ingredient[]) => {
           this.ingredients = ingredients;
         }
-      )
+      );
   }
 }
