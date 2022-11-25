2c0b59d1e8779adcb9713f1a0b3588ea74fc9c78
Elena Krikun <elena.krikun@quantori.com>
2022-11-25 20:45:52 +0400

    added firebase

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,32 +8,18 @@ import {Subject} from "rxjs";
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
   
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Tasty Schnitzel',
-      'Test',
-      'https://i.pinimg.com/736x/57/d1/a8/57d1a885b3b161e8ecee35e194bed3a8.jpg',
-      [
-        new Ingredient('meat', 1),
-        new Ingredient('french freis', 2),
-      ]
-    ),
-    new Recipe(
-      'Big Fat Burger',
-      'Test',
-      'https://i.pinimg.com/736x/57/d1/a8/57d1a885b3b161e8ecee35e194bed3a8.jpg',
-      [
-        new Ingredient('breat', 2),
-        new Ingredient('meat', 2),
-      ]
-    )
-  ];
+  private recipes: Recipe[] = [];
   
   constructor(private  slService: ShoppingListService) {
   }
   
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+  
   getRecipes() {
-    return this.recipes.slice();
+    return this.recipes;
   }
   
   getRecipe(id: number) {
@@ -57,6 +43,5 @@ export class RecipeService {
   deleteRecipe(index: number) {
     this.recipes.splice(index, 1);
     this.recipesChanged.next(this.recipes.slice());
-  
   }
 }
