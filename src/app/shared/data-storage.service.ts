8dd8dfa971def01c567ec77f3cbf864df658474a
Elena Krikun <elena.krikun@quantori.com>
2022-12-13 21:39:28 +0400

    auth fixes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipes.model";
-import { map, tap } from "rxjs/operators";
+import { map, tap, take, exhaustMap } from "rxjs/operators";
+import { AuthService } from "../auth/auth.service";
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
   constructor(private http: HttpClient,
-              private  recipeService: RecipeService) {
+              private  recipeService: RecipeService,
+              private authService: AuthService) {
   }
   
   storeRecipes() {
@@ -19,18 +21,19 @@ export class DataStorageService {
   }
   
   fetchRecipes() {
-    return this.http.get<Recipe[]>('https://recipe-book-749ec-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
-      .pipe(
-        map(recipes => {
+     return this.http.get<Recipe[]>(
+          'https://recipe-book-749ec-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+        ).pipe(map(recipes => {
         return recipes.map(recipe => {
-          return {
-            ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []
-          }
-        })
-      }),
+        return {
+          ...recipe,
+          ingredients: recipe.ingredients ? recipe.ingredients : []
+        };
+      });
+    }),
       tap(recipes => {
-        this.recipeService.setRecipes(recipes)
+        this.recipeService.setRecipes(recipes);
       })
-      )
+    )
   }
 }
