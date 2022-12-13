ddddfcc929df7f15b8424b6bd795b19a35a528c2
Elena Krikun <elena.krikun@quantori.com>
2022-12-13 21:08:41 +0400

    auth in the process

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataStorageService } from "../shared/data-storage.service";
+import { AuthService } from "../auth/auth.service";
+import { Subscription } from "rxjs/Subscription";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
   collapsed = true;
-  constructor(private dataStorageService: DataStorageService) {}
+  private userSub: Subscription;
+  isAuthenticated = false;
+  
+  constructor(private dataStorageService: DataStorageService, private authService: AuthService) {}
+  
+  ngOnInit() {
+    this.userSub = this.authService.user.subscribe(user => {
+      this.isAuthenticated = !!user;
+    });
+  }
   
   onSaveData() {
     this.dataStorageService.storeRecipes();
@@ -15,5 +26,9 @@ export class HeaderComponent {
   
   onFetchData() {
     this.dataStorageService.fetchRecipes().subscribe();
+  }
+  
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
   }
 }
